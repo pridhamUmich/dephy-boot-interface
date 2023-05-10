82f8646bce772f1574e0fe564c7db0bfc970e9b0
paul <pridham@umich.edu>
2023-05-10 12:41:52 -0400

    created a zero torque controller

---
+++ Code/ZeroTorque.py
@@ -0,0 +1,110 @@
+from Controller import *
+
+# P. Pridham, May 2023 
+
+class ZeroTorque(Controller) : 
+    """ 
+    A class to apply a zero torque controller
+
+    ...
+
+    Attributes
+    ----------
+    None
+
+    Methods
+    -------
+    set_parameters(parameters):
+        Does nothing, here for compatibility with other controllers
+        
+    calculate_torque_cmd(state_info):
+        Returns a 0 torque command
+    """
+    def __init__(self) : 
+        """
+        Constructs all the necessary attributes for the ZeroTorque Controller. 
+
+        Parameters
+        ----------
+        None
+            
+        Returns
+        -------
+        None   
+        
+        """
+        super().__init__()
+        
+        
+    
+    
+    def set_parameters(self, parameters) :
+        """
+        Calculates the spline parameters a-d for the curve so we don't need to redo this every loop.  Should be called when the nature of the curve changes.  If all the curve parameters are not set (!=-1) it will not recalculate.
+
+        Parameters
+        ----------
+        parameters : dict
+            Parameters for the Zero torque profile, keys :
+                None
+             
+        Returns
+        -------
+        None   
+        """
+        pass
+               
+    def calculate_torque_cmd(self, state_info) : 
+        """
+        Calculates the current torque command based on the supplied state info, empty. 
+
+        Parameters
+        ----------
+        state_info : dict
+            State info from the exo and person used to calculate the Zhang/Collins torque profile, keys :
+                None
+                
+            
+        Returns
+        -------
+        torque_cmd : float  
+            0 Nm.
+        """
+        self.torque_cmd = 0 # store the torque command.
+        return self.torque_cmd # return the torque command.
+
+if __name__ == '__main__':
+    import numpy as np
+    import matplotlib.pyplot as plt
+    
+    # create the ZhangCollins instance
+    controller = ZeroTorque()
+    # create the parameter dictionary for input.
+    parameters = {
+        
+    }
+    
+    controller.set_parameters(parameters)
+    
+    percent_gait = np.linspace(0,100,101) # create a set of percent gaits to calculate torque for.
+    torque_cmd = [] # create a place to store the torque command output for printing.
+    # Initialize a state for the system 
+    state_info = {
+        "percent_gait" : -1
+    }
+    
+    for p in percent_gait : # iterate through the different values of percent_gait.
+        state_info["percent_gait"] = p # set the value in state info to be the current values we are using.
+        torque_cmd.append(controller.calculate_torque_cmd(state_info)) # append the newly calculated value to the end of the torque_cmd.
+        # print(f'ZhangCollins :: __main__ : percent_gait {p} -> torque_cmd {zhang_collins.calculate_torque_cmd([p])}')  # debug statement.
+    # print(torque_cmd) # debug statement.
+    
+    fig, ax = plt.subplots() # create a subplot
+    ax.plot(percent_gait, torque_cmd) # plot the torque command
+
+    # add labels to the plot
+    ax.set(xlabel='Percent Gait', ylabel='Torque (Nm)',
+           title='Torque Profile')
+    
+    fig.savefig("ZeroTorqueTest.png") # save the plot
+    plt.show() # display the plot
