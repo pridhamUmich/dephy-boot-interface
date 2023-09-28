d9573617972e0f94000e2c33d25a75d7ac4e0806
Man I Wu <maniwu@umich.edu>
2023-09-28 14:10:49 -0400

    added toeoff segmentation and expected stance duration

--- Code/GaitCalculator.py
+++ Code/GaitCalculator.py
@@ -28,9 +28,11 @@ class GaitCalculator:
     _toeoff_timestamp_previous : int
         Previous toeoff timestamp 
     _num_stride_times_to_avg : int
-        Number of step durations averaged to estimate expected_step_duration_ms
+        Number of stride durations averaged to estimate expected_stride_duration_ms
     _past_stride_durations : list 
-        List of previous step durations used to estimate expected_step_duration_ms
+        List of previous stride durations used to estimate expected_stride_duration_ms
+     _past_stance_durations : list 
+        List of previous stance durations used to estimate expected_stance_duration_ms
 
     Input Parameters
     ----------------
@@ -66,8 +68,10 @@ class GaitCalculator:
         self._heelstrike_armed_duration_percent_gait = heelstrike_armed_duration_percent_gait
         self._num_strides_to_avg = num_strides_to_avg 
         self._past_stride_durations = [-1] * self._num_strides_to_avg
+        self._past_stance_durations = [-1] * self._num_strides_to_avg
         
         self._heelstrike_armed = False
+        self._toeoff_armed = False
         self._heelstrike_armed_timestamp_ms = -1
         self._toeoff_armed_timestamp_ms = -1
         
@@ -81,8 +85,8 @@ class GaitCalculator:
     def calculate_gait_parameters(self, timestamp_ms, gyro_rad_s):
         self._check_for_heelstrike(timestamp_ms, gyro_rad_s)
         self._check_for_toeoff(timestamp_ms, gyro_rad_s)
-        self._calc_percent_gait()
-        self._calc_percent_stance()
+        self._calc_percent_gait(timestamp_ms)
+        self._calc_percent_stance(timestamp_ms)
         
         return {
             'percent_gait' : self._percent_gait,
@@ -91,14 +95,21 @@ class GaitCalculator:
             'toeoff_trigger' : self._toeoff_trigger,
         }
     
+    def clear_gait_estimate(self):
+        self._expected_stance_duration_ms = -1
+        self._expected_stride_duration_ms = -1
+        self._past_stride_durations = -1 * self._num_strides_to_avg
+    
     def _check_for_heelstrike(self, timestamp_ms, gyro_rad_s):
         """
-        DESCRIPTION
-        TODO : see if there are any updates we want to make to be compatible with toeoff check.
+        Detects heelstrike based on whether gyro_rad_s exceeds the heelstrike segmentation threshold. 
 
         Parameters
         ----------
-        None   
+        timestamp_ms : int
+            Current timestamp
+        gyro_rad_s : float
+            Gyroscope data in rad/s (suggested gyro_z)
             
         Returns
         -------
@@ -111,56 +122,56 @@ class GaitCalculator:
         armed_time = 0
         if self._heelstrike_armed_timestamp_ms != -1 :
             armed_time = timestamp_ms - self._heelstrike_armed_timestamp_ms
-        if ((not self.heelstrike_armed) and gyro_rad_s >= self._heelstrike_segmentation_arm_threshold_rad_s) :
+        if ((not self._heelstrike_armed) and gyro_rad_s >= self._heelstrike_segmentation_arm_threshold_rad_s) :
             self._heelstrike_armed = True
             self._heelstrike_armed_timestamp_ms = timestamp_ms
         if (self._heelstrike_armed and (gyro_rad_s <= self._heelstrike_segmentation_arm_threshold_rad_s)) :
-            self.heelstrike_armed = False
+            self._heelstrike_armed = False
             self._heelstrike_armed_timestamp_ms = -1 
-            if  (armed_time > self._heelstrike_armed_duration_percent_gait/100 * self._expected_step_duration_ms) :
+            if  (armed_time > self._heelstrike_armed_duration_percent_gait/100 * self._expected_stride_duration_ms) :
                 triggered = True
-                # update heelstrike timestamps and expected step duration 
-                self._update_expected_duration(timestamp_ms)
+                # update heelstrike timestamps and expected stride duration 
+                self._update_expected_stride_duration(timestamp_ms)
             
         self.heelstrike_trigger = triggered
-        return self.heelstrike_trigger
-        
+        # return self.heelstrike_trigger
     
-    def _check_for_toeoff(self, gyro_rad_s):
+    def _check_for_toeoff(self, timestamp_ms, gyro_rad_s):
         """
-        DESCRIPTION
-        TODO : Need to figure out algorithm to use.
+        Detects toeoff using sign inversion of gyro_rad_s from positive to negative (or flipped according to convention). 
 
         Parameters
         ----------
-        None   
+        timestamp_ms : int
+            Current timestamp
+        gyro_rad_s : float
+            Gyroscope data in rad/s (suggested gyro_z)
             
         Returns
         -------
         None
         """
-        # the trigger on the inversion of the leg is one method.
-        # can also use spikes in acceleration (X seems to be best candidate but may not work well for slower gaits with smaller impacts)
-        # Other candidates also possible.
-        # triggered = False
-        # armed_time = 0
-        # if self.armed_timestamp != -1 :
-            # armed_time = self.data_current[self.idx_time] - self.armed_timestamp
-        # if ((not self.heelstrike_armed) and self.data_current[self.idx_gyro_z] >= self.segmentation_arm_threashold) :
-            # self.heelstrike_armed = True
-            # self.armed_timestamp = self.data_current[self.idx_time]
-        # if (self.heelstrike_armed and (self.data_current[self.idx_gyro_z] <= self.segmentation_trigger_threashold) ) :
-            # self.heelstrike_armed = False
-            # self.armed_timestamp = -1
-            # if  (armed_time > ARMED_DURATION_PERCENT/100 * self.expected_duration) :
-                # triggered = True
-            
-            
-        # self.segmentation_trigger = triggered
-        return -1
-    
-    def _update_expected_duration(self, timestamp_ms):
+        # Check for gyro sign change from positive to negative (or flipped? check sign convention)
 
+        triggered = False
+        armed_time = 0
+        if self._toeoff_armed_timestamp_ms != -1 :
+            armed_time = timestamp_ms - self._toeoff_armed_timestamp_ms
+        if ((not self._toeoff_armed) and (gyro_rad_s <= 0)) :
+            self._toeoff_armed = True
+            self._toeoff_armed_timestamp_ms = timestamp_ms
+        if (self._toeoff_armed and (gyro_rad_s > 0) ) :
+            self._toeoff_armed = False
+            self._toeoff_armed_timestamp_ms = -1
+            if (armed_time > self._heelstrike_armed_duration_percent_gait/100 * self._expected_stride_duration_ms) :
+                triggered = True
+                # update toeoff timestamps and expected stance duration 
+                self._update_expected_stance_duration(timestamp_ms)
+            
+        self.toeoff_trigger = triggered
+        # return self.toeoff_trigger
+    
+    def _update_expected_stride_duration(self, timestamp_ms):
         if (self._heelstrike_timestamp_previous == -1):
             self._heelstrike_timestamp_previous = timestamp_ms  # store the first heelstrike timestamp 
             return 
@@ -179,8 +190,29 @@ class GaitCalculator:
             # calculate average of previous stride durations and update expected stride duration 
             self._expected_stride_duration_ms = sum(self._past_stride_durations) / self._num_strides_to_avg
     
+    def _update_expected_stance_duration(self, timestamp_ms):
+        if (self._toeoff_timestamp_previous == -1):
+            self._toeoff_timestamp_previous = timestamp_ms
+            return
+        
+        self._toeoff_timestamp_previous = self._toeoff_timestamp_current
+        self._toeoff_timestamp_current = timestamp_ms
+        stance_duration = self._toeoff_timestamp_current - self._heelstrike_timestamp_current
+
+        if (-1 in self._past_stance_durations):
+            self._past_stance_durations.insert(0, stance_duration)
+            self._past_stance_durations.pop()
+        elif (stance_duration >= 0.25 * min(self._past_stance_durations) and 
+              stance_duration >= 1.75 * max(self._past_stance_durations)):
+            self._past_stance_durations.insert(0, stance_duration)
+            self._past_stance_durations.pop()
+
+            self._expected_stance_duration_ms = sum(self._past_stance_durations) / self._num_strides_to_avg
+
     def _calc_percent_gait(self, timestamp_ms):
-        pass
+        time_elapsed = timestamp_ms - self._heelstrike_timestamp_current
+        self.percent_gait = time_elapsed / self._expected_stride_duration_ms * 100
+        self.percent_gait = max(100, self.percent_gait) # cap percent_gait at 100 
 
     def _calc_percent_stance(self, timestamp_ms):
-        pass
+        return -1
